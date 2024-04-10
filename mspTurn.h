764412fe1d7f04b757260597b4f5994d47f7a1cb
Tom-Hao <58838926+Tom-Hao@users.noreply.github.com>
2024-04-10 16:38:20 +0800

    Add files via upload

---
+++ mspTurn.h
@@ -0,0 +1,9 @@
+#ifndef MSP_TURN
+#define MSP_TURN
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+void mspTurn(unsigned char** mspData, unsigned char** bmpData, int width, int height);
+
+#endif
