2168a6e36b827391f8a14b4feb8b58544eef03d6
Tom-Hao <58838926+Tom-Hao@users.noreply.github.com>
2024-04-11 15:27:56 +0800

    Delete mspTurn.h

--- mspTurn.h
+++
@@ -1,9 +0,0 @@
-#ifndef MSP_TURN
-#define MSP_TURN
-#include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
-
-void mspTurn(unsigned char** mspData, unsigned char** bmpData, int width, int height);
-
-#endif
