1b991032581e1a2d571fc5f1505191aa7d55dcde
Tom-Hao <58838926+Tom-Hao@users.noreply.github.com>
2024-04-11 15:27:14 +0800

    Delete bmpFiller.h

--- bmpFiller.h
+++
@@ -1,9 +0,0 @@
-#ifndef BMPFILLER
-#define BMPFILLER
-#include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
-
-int bmpFiller(FILE* fp, unsigned char** bmpData, int width, int height, int lineBytes);
-
-#endif
